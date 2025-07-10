d4620e08d5ebe377c4ab4d8968bf743bbddd0fa5
shivamkumar93 <bcashivam11@gmail.com>
2025-07-10 17:02:23 +0530

    register, login authentication work is down

--- mytodo/firsttodo/views.py
+++ mytodo/firsttodo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import *
-from .models import *
+from firsttodo.forms import *
+from firsttodo.models import *
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout,authenticate
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def todo_list(request):
     data = {}
@@ -12,12 +16,19 @@ def todo_list(request):
     data['tests'] = test
     return render(request, 'todo/index.html', data)
 
+@login_required
 def create_todo(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
         Todo.objects.create(title=title, description=description)
     return redirect('todo')
+
+def home(request):
+    data = {}
+    test = Todo.objects.all()
+    data['tests'] = test
+    return render(request, 'todo/dashboard.html',data)
 
 def delete_todo(request, todo_id):
     todo = Todo.objects.get(id = todo_id)
@@ -30,3 +41,31 @@ def complete_todo(request, todo_id):
     todo.completed  = True
     todo.save()
     return redirect('todo')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('loginpage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/register.html',{'form':form})
+
+def auth_login(r):
+    form = AuthenticationForm(r.POST or None)
+    if r.method == "POST":
+        username = r.POST.get('username')
+        password = r.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(r, user)
+            return redirect('todo')
+    data = {}
+    data['form'] = form
+    return render(r, 'auth/login.html', data)
+
+def logout_view(request):
+    logout(request)
+    return redirect("loginpage")
--- mytodo/mytodo/urls.py
+++ mytodo/mytodo/urls.py
@@ -5,8 +5,11 @@ from firsttodo.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", todo_list, name="todo"),
+    path("", home, name="dashboard"),
+    path("todo/", todo_list, name="todo"),
     path("create/", create_todo, name="create_todo"),
     path("completed/<int:todo_id>/", complete_todo, name="completed_todo"),
     path("delete/<int:todo_id>/", delete_todo, name="delete"),
+    path("register/", register_view, name="registerpage"),
+    path("login/", auth_login, name="loginpage"),
 ]
